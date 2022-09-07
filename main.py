d9aaf8794fb9811462bf4e6e5e8d9f0931459ca6
Akhtar <akhtar729@gmail.com>
2022-09-07 06:16:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+print("hello world")
+print("Here is another example")
+print("Now i have to learn how to use replit")
+
+age = 20
+print(age)
+
+is_hot = True
+if is_hot == False:
+  print("Today is very hot")
+else:
+  print("Nice day to go out with friends")
+
+name = "John Smith"
+age1 = 30 
+is_new_patient = True
+
+if is_new_patient == True:
+  print(f"{name} is {age} old and a new patient")
+else:
+  print(f"{name} is {age} old and an old patient")
